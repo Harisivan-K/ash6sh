dbe6ab7b7915587843b49ec750ce460581bede35
Harisivan-K <sivaharimunnar@gmail.com>
2026-07-09 20:15:31 +0530

    Make Project fields optional

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
 
-
-
 class Project(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
+    title = models.CharField(
+        max_length=200,
+        blank=True,
+        null=True
+    )
+
+    description = models.TextField(
+        blank=True,
+        null=True
+    )
 
     thumbnail = models.ImageField(
         upload_to="thumbnails/",
@@ -22,4 +28,4 @@ class Project(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title
+        return self.title if self.title else "Untitled Project"
